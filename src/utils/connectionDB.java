2e2789330e869ef17aa1b48c19b0e54d83b4f7c3
Marco Carrillo <maurecar@gmail.com>
2021-09-14 22:11:30 -0500

    update base datos2

--- src/utils/connectionDB.java
+++ src/utils/connectionDB.java
@@ -17,7 +17,7 @@ public class connectionDB {
         Connection conn = null;
         String host = "localhost";
         String user = "root";
-        String password = "xxxxxx";
+        String password = "xxxxxx";//aqui poner su clave de base de datos
         String port = "3306";
         String db = "reto4";
         String dbURL = "jdbc:mysql://" + host + ":" + port + "/" + db;
